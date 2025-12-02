4ab509c5ba913fadcdad47362ac967ed46849a7c
Minamaher-sw <mosadefmena185@gmail.com>
2025-12-02 17:28:09 +0200

    feat: add server configuration with port and auto-open options in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4200,   // <--- change port here
+    open: true    // optional: auto-open browser
+  }
 })
